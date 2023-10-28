b0598bf6f23a2e4b5c302193ea81c8ef5ea31335
Nastassial <nastassialmezhen@gmail.com>
2023-10-28 15:33:14 +0300

    Добавьте файлы проекта.

---
+++ HttpClientGetDomains/Classes/HttpClientManager.cs
@@ -0,0 +1,32 @@
+﻿namespace HttpClientGetDomains.Classes;
+
+internal class HttpClientManager
+{
+    private readonly HttpClient _httpClient = new HttpClient();
+
+    private readonly string _url;
+
+    public string Url { get { return _url; } }
+
+    public HttpClientManager(string url) 
+    {
+        _url = url;
+    }
+
+    public async Task<string> GetContent()
+    {
+        HttpResponseMessage httpResponse = await _httpClient.GetAsync(Url);
+
+        if (httpResponse.IsSuccessStatusCode)
+        {
+            string htmlContent = await httpResponse.Content.ReadAsStringAsync();
+
+            if (!string.IsNullOrEmpty(htmlContent))
+            {
+                return htmlContent;
+            }
+        }
+
+        return null;
+    }
+}
---
+++ HttpClientGetDomains/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+string pathDirectory = Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.FullName ?? "../../../";
+string filePath = Path.Combine(pathDirectory, "Data/htmlDomainPages.txt");
+//string filePath = Path.Combine(pathDirectory, "Data/");
+
+if (File.Exists(filePath))
+{
+    File.Delete(filePath);
+}
+
+int domainPagesCnt = 0;
+
+StringBuilder domain = new StringBuilder();
+
+const string url = "https://ru.wikipedia.org/wiki/";
+
+char[] letters = Enumerable.Range('a', 'z' - 'a' + 1).Select(c => (char)c).ToArray(); // sequence "abcdefghijklmnopqrstuvwxyz"
+
+for (int i = 0; i < letters.Length; i++)
+{
+    for (int j = 0; j < letters.Length; j++)
+    {
+        domain.Clear().Append('.').Append(letters[i]).Append(letters[j]);
+
+        HttpClient client = new HttpClient();
+
+        HttpResponseMessage httpResponse = await client.GetAsync($"{url}{domain}");
+
+        if (httpResponse.IsSuccessStatusCode)
+        {
+            Console.WriteLine(domain.ToString());
+            
+            string htmlContent = await httpResponse.Content.ReadAsStringAsync();
+
+            if (!string.IsNullOrEmpty(htmlContent))
+            {
+                File.AppendAllText(filePath, $"\n\n--- PAGE FOR DOMAIN {domain} ---\n\n" + htmlContent);
+
+                //File.WriteAllText(filePath + $"{domain}.html", htmlContent);
+            }
+
+            domainPagesCnt++;
+        }
+    }
+}
+
+Console.WriteLine($"Number of html pages - {domainPagesCnt}");
